10edc743edbf49230792f083a105a3fa6110ed6d
KPCCoiL <4506345+KPCCoiL@users.noreply.github.com>
2026-05-31 15:57:14 +0900

    Upgrade to nixpkgs-26.05

--- flake.nix
+++ flake.nix
@@ -2,11 +2,11 @@
   description = "nix-darwin configuration for InternalBlaze";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-25.11-darwin";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-26.05-darwin";
     nixpkgsUnstable.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
-    nix-darwin.url = "github:nix-darwin/nix-darwin/nix-darwin-25.11";
+    nix-darwin.url = "github:nix-darwin/nix-darwin/nix-darwin-26.05";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-    home-manager.url = "github:nix-community/home-manager/release-25.11";
+    home-manager.url = "github:nix-community/home-manager/release-26.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     vis-tmux-repl = {
